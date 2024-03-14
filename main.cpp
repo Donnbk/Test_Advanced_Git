090c4296309e68e86b76d442d15e501bd29f8693
Donnbk <ngongocdobk@gmail.com>
2024-03-14 08:51:30 +0700

    Update file

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int x,y,sum;
+
+	cout << "Input values of x and y:" << "\n";
+	cin >> x >> y;
+
+	sum = x + y;
+	cout << "sum = " << sum << "\n";
+	
+	system("pause");
+	return 0;
+}
