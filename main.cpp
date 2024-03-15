54f18e2df8c4666cfbb24b79d324ab38e8d825da
Donnbk <ngongocdobk@gmail.com>
2024-03-15 09:19:03 +0700

    Edit variable name

--- main.cpp
+++ main.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 
 int main(){
-	int x,y,sum10;
+	int x,y,sum_xy;
 
 	cout << "Input values of x and y:" << "\n";
 	cin >> x >> y;
 
-	sum10 = x + y;
-	cout << "sum = " << sum10 << "\n";
+	sum_xy = x + y;
+	cout << "sum = " << sum_xy << "\n";
 	
 	return 0;
 }
